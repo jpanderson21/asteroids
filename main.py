f9104fa26e3a2a5400c9468b414aba3d092327fc
JP Anderson <jpanderson2014@icloud.com>
2024-09-24 15:31:50 -0400

    Initial commit. Imports pygame and creates an empty window.

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+
+
+def main():
+    # set up
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # main loop
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
